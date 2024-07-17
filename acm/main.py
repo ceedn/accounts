86b656a0ce672439d3b7f2237169cea7b6863be1
ACFC <acfc@acfc.dev>
2024-07-17 16:15:13 -0400

    update

--- acm/main.py
+++ acm/main.py
@@ -8,7 +8,7 @@ start_time = datetime.datetime.now()
 async def read_root(request: Request):
     return {"message": "Hello World!!!!"}
 
-@app.get("/measurements")
+@app.get("/accounts/measurements")
 async def read_uptime(request: Request):
     uptime = datetime.datetime.now() - start_time
     return {"uptime": uptime}
