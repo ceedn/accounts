9fdac686520824da0aeb9cb5d5acbdbcf7918188
ACFC <acfc@acfc.dev>
2024-07-17 16:09:19 -0400

    update

--- acm/main.py
+++ acm/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, Request
+import datetime
 
 app = FastAPI()
+start_time = datetime.datetime.now()
 
 @app.get("/")
 async def read_root(request: Request):
     return {"message": "Hello World!!!!"}
 
 @app.get("/measurements")
-async def read_root(request: Request):
-    return {"success": True}
+async def read_uptime(request: Request):
+    uptime = datetime.datetime.now() - start_time
+    return {"uptime": uptime}
