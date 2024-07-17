e9460398095c75d962dcf0aac3096a8c21b691c2
ACFC <acfc@acfc.dev>
2024-07-17 15:59:43 -0400

    Add a measurements endpoint to test CI testing

--- acm/main.py
+++ acm/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root(request: Request):
     return {"message": "Hello World!!!!"}
+
+@app.get("/measurements")
+async def read_root(request: Request):
+    return {"success": True}
